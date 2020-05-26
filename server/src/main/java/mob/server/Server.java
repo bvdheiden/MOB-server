318d340ea6aad6a123ab6bef6602963dfb470d3b
Ben van der Heiden <b.vdh@protonmail.com>
2020-05-26 11:40:42 +0200

    Initial commit

---
+++ server/src/main/java/mob/server/Server.java
@@ -0,0 +1,12 @@
+package mob.server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Server");
+        primaryStage.show();
+    }
+}
