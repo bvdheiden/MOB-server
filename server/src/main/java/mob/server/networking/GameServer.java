6ff06580eee0d9b80b224185d18d37e2b903ea17
Ben van der Heiden <b.vdh@protonmail.com>
2020-05-26 12:46:19 +0200

    Add GameServer.java

---
+++ server/src/main/java/mob/server/networking/GameServer.java
@@ -0,0 +1,83 @@
+package mob.server.networking;
+
+import mob.sdk.networking.SocketClient;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class GameServer {
+    private static final int SERVER_PORT = 10_000;
+
+    private final AtomicBoolean connecting = new AtomicBoolean(false);
+    private final List<SocketClient> socketClientList = new CopyOnWriteArrayList<>();
+    private ServerSocket serverSocket;
+
+
+    /**
+     * Start the server socket and start listening for clients.
+     */
+    public void start() {
+        if (isRunning()) {
+            return;
+        }
+
+        connecting.set(true);
+
+        new Thread(() -> {
+            try {
+                SocketClient.print("Creating server socket.");
+                this.serverSocket = new ServerSocket(SERVER_PORT);
+                while (connecting.get()) {
+                    try {
+                        SocketClient.print("Waiting for clients.");
+                        Socket socket = serverSocket.accept();
+                        SocketClient.print("Client accepted.");
+                        SocketClient client = new SocketClient(socket);
+                        socketClientList.add(client);
+
+                        client.addTransactionListener((transaction) -> {
+
+                        });
+
+                        client.addDisconnectionListener(() -> {
+
+                        });
+                    } catch (IOException exception) {
+                        SocketClient.print("Failed to accept and create client.");
+                    }
+                }
+            } catch (IOException exception) {
+                SocketClient.print("Failed to create server socket.");
+            }
+        }).start();
+    }
+
+    /**
+     * Stop the server socket and stop listening for clients.
+     */
+    public void stop() {
+        if (!isRunning()) {
+            return;
+        }
+
+        connecting.set(false);
+
+        for (SocketClient serverClient : socketClientList) {
+            serverClient.stop();
+        }
+
+        try {
+            serverSocket.close();
+        } catch (IOException exception) {
+            SocketClient.print("Failed to close server socket.");
+        }
+    }
+
+    public boolean isRunning() {
+        return connecting.get() || (serverSocket != null && !serverSocket.isClosed());
+    }
+}
