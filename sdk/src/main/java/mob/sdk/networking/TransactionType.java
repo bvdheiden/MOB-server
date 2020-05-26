e0c898f1dafe090227e953e38ab1390f8ea2cd04
Ben van der Heiden <b.vdh@protonmail.com>
2020-05-26 12:50:39 +0200

    Add TransactionType

--- sdk/src/main/java/mob/sdk/networking/TransactionType.java
+++ sdk/src/main/java/mob/sdk/networking/TransactionType.java
@@ -1,14 +1,5 @@
 package mob.sdk.networking;
 
 public enum TransactionType {
-    PLAYER_LOGIN,
-    PLAYER_CONNECTED,
-    PLAYER_DISCONNECTED,
-    PLAYER_READY,
-    PLAYER_POSITION,
-    ROOM_CONNECT,
-    ROOM_CONNECTED,
-    ROOM_DISCONNECT,
-    ROOM_DISCONNECTED,
-    GAME_STARTED,
+
 }
