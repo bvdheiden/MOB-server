9e473e1ad6ceb33ee27c16735fa0d6ddef6f703a
Ben van der Heiden <b.vdh@protonmail.com>
2020-05-26 16:12:25 +0200

    Add comparable

--- sdk/src/main/java/mob/sdk/cards/Card.java
+++ sdk/src/main/java/mob/sdk/cards/Card.java
@@ -2,72 +2,46 @@ package mob.sdk.cards;
 
 import java.io.Serializable;
 
-public class Card implements Serializable {
-    private CardType cardType;
-    private CardIdentifier identifier;
+public class Card implements Serializable, Comparable<Card> {
+    private final CardType cardType;
+    private final CardIdentifier identifier;
 
-    public Card(CardType cardType,CardIdentifier cardIdentifier) {
+    public Card(CardType cardType, CardIdentifier identifier) {
         this.cardType = cardType;
         this.identifier = identifier;
     }
 
-    /**
-     * attacks the other player's card
-     * @param other the other player's card
-     * @return 0 if it was a draw, 1 if you won and -1 if you lost
-     */
-    public int attack(Card other) {
-        int res = 0;
-        if (other != null) {
-            if (other.getCardType() != null) {
-                if (other.getCardType() == this.getCardType()) {
-                    return 0;
-                }
-
-                switch (this.cardType) {
-                    case BOW -> {
-                        if (other.getCardType() == CardType.SWORD) {
-                            return -1;
-                        } else if (other.getCardType() == CardType.SHIELD) {
-                            return 1;
-                        }
-
-                    }
-                    case SWORD -> {
-                        if (other.getCardType() == CardType.BOW) {
-                            return 1;
-                        } else if (other.getCardType() == CardType.SHIELD) {
-                            return -1;
-                        }
-                    }
-                    case SHIELD -> {
-                        if (other.getCardType() == CardType.BOW) {
-                            return -1;
-                        } else if (other.getCardType() == CardType.SWORD) {
-                            return 1;
-                        }
-                    }
-                    default -> res = 0;
-                }
-            }
-        }
-        return res;
-    }
-
-
     public CardType getCardType() {
         return cardType;
-    }
-
-    public void setCardType(CardType cardType) {
-        this.cardType = cardType;
     }
 
     public CardIdentifier getIdentifier() {
         return identifier;
     }
 
-    public void setIdentifier(CardIdentifier identifier) {
-        this.identifier = identifier;
+    @Override
+    public int compareTo(Card o) {
+        switch (cardType) {
+            case BOW:
+                if (o.cardType == CardType.SWORD)
+                    return -1;
+                if (o.cardType == CardType.SHIELD)
+                    return 1;
+                break;
+            case SWORD:
+                if (o.cardType == CardType.BOW)
+                    return -1;
+                if (o.cardType == CardType.SHIELD)
+                    return 1;
+                break;
+            case SHIELD:
+                if (o.cardType == CardType.BOW)
+                    return -1;
+                if (o.cardType == CardType.SWORD)
+                    return 1;
+                break;
+        }
+
+        return 0;
     }
 }
