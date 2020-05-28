bfcc2ef597c0f958238da56f633528cae15a162e
Sem van der Hoeven <semmer99@gmail.com>
2020-05-28 12:28:25 +0200

    made identifiers into strings

--- sdk/src/main/java/mob/sdk/cards/CardIdentifier.java
+++ sdk/src/main/java/mob/sdk/cards/CardIdentifier.java
@@ -1,11 +1,15 @@
 package mob.sdk.cards;
 
 public enum  CardIdentifier {
-    PARFOES,    //tovenaar
-    KEVIN,      //knecht
-    BARRY,      //barbaar
-    AVANIUS     //koning
+    PARFOES("parfoes de tovenaar"),    //tovenaar
+    KEVIN("kevin de knecht"),      //knecht
+    BARRY("barry de barbaar"),      //barbaar
+    AVANIUS("koning avanius")     //koning
+    ;
     //TODO add more cards
 
 
+    CardIdentifier(String s) {
+
+    }
 }
