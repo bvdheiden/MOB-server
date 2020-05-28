da6512a3ff25479b9d9999c95a02ca2e87a9573a
Sem van der Hoeven <semmer99@gmail.com>
2020-05-28 10:38:44 +0200

    add card request transaction types

--- sdk/src/main/java/mob/sdk/networking/TransactionType.java
+++ sdk/src/main/java/mob/sdk/networking/TransactionType.java
@@ -3,5 +3,7 @@ package mob.sdk.networking;
 public enum TransactionType {
     BATTLE_REQUEST,
     BATTLE_REQUEST_INVALID,
-    BATTLE_RESULT
+    BATTLE_RESULT,
+    CARD_REQUEST,
+    CARD_REQUEST_INVALID
 }
